8f4940c59b98075f2cdcd09d251477d2193752c9
Shane Chellam <shanechellamsp@gmail.com>
2024-05-16 11:17:35 +0530

    responsive and contact form

--- main.js
+++ main.js
@@ -33,3 +33,53 @@ menuIcon.classList.remove('bx-x');
 navbar.classList.remove('active');
 };
 
+//Scroll Reveal//
+
+ScrollReveal({ 
+    reset: true,
+    distance: '80px',
+    duration: 1500,
+    delay:100
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.services-container, .contact', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img img', { origin: 'left' });
+ScrollReveal().reveal('.home-content h3, .home-content p, .about-content', { origin: 'right' });
+
+
+const form = document.querySelector("form");
+const fullName = document.getElementById("name");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const sub = document.getElementById("subject");
+const mess = document.getElementById("message");
+
+function sendEmail(){
+    const bodyMessage = `Full Name: ${fullName.value}<br> Email: ${email.value}<br> Phone Number: ${phone.value}<br> Subject: ${sub.value}<br> Message: ${mess.value}`;
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "shanechellamsp@gmail.com",
+        Password : "7C0F93BF3EA99DC9508DB08B5E99FB2668BB",
+        To : "shanechellamsp@gmail.com",
+        From : "shanechellamsp@gmail.com",
+        Subject : sub.value,
+        Body : bodyMessage
+    }).then(
+      message => {
+        if(message=="OK"){
+            Swal.fire({
+                title: "Good job!",
+                text: "Message sent Successfully!",
+                icon: "success"
+              });
+        }
+      }
+    );
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    sendEmail();
+})
